4fccbe90381fd368bf0b6d57b4867bc8ede5096e
enganisamohamed <enganisamohamed@gmail.com>
2026-03-11 11:53:49 -0700

    adding exercise 17

--- mainjs16.js
+++
@@ -1,6 +0,0 @@
-let score = 60;
-if( score >= 50){
-    console.log("your passed")
-} else{
-    console.log("your failed")
-}
---
+++ mainjs17.js
@@ -0,0 +1,18 @@
+let tempreture = 30;
+if (tempreture < 0){
+
+    console.log('very  cold')
+}
+else if (tempreture >= 0 && tempreture <= 15) {
+    console.log ("cold")
+}
+
+else if (tempreture >= 15 && tempreture <= 25){
+
+    console.log ("worm")
+
+}
+else {
+    console.log("hot")
+
+}
